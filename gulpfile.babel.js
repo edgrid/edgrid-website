1b3151ba2492d4f16c9d8aeb995aaf29aadc746a
Johelyn <johelyn.cordova@gmail.com>
2018-01-01 16:09:47 -0500

    lading, modificacion del logo del nav

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,6 +12,11 @@ import source from 'vinyl-source-stream';
 import sourcemaps from 'gulp-sourcemaps';
 import buffer from 'vinyl-buffer';
 
+
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+var notify = require('gulp-notify');
+
 const server = browserSync.create();
 
 const postcssPlugins = [
@@ -62,14 +67,42 @@ gulp.task('scripts', () =>
     .pipe(gulp.dest('./public/js'))
 );
 
+gulp.task('images', function() {
+ gulp.src('./dev/images/**/*.{png,jpg,jpeg,gif}')//Ruta a la carpeta images a puntando a las imágenes 
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngcrush()]
+  }))
+  .pipe(gulp.dest('./public/images')) //Carpeta donde se guardaran las imágenes comprimidas
+  .pipe(notify("La tarea images a culminado!"));//Mensaje gracias a `gulp-notify`
+});
+
+//Vuelve a ejecutar la tarea cuando se modifica algún archivo 
+gulp.task('watch', function(){
+  gulp.watch('./dev/images/**/*', ['images']);
+});
+
+
+gulp.task('copy', function() {
+ gulp.src('./dev/images/**/*.svg')
+  .pipe(gulp.dest('./public/images'))
+});
+
+gulp.task('copy', function(){
+  gulp.watch('./dev/images/**/*.svg', ['copy']);
+});
+
 gulp.task('default', () => {
   server.init({
     server: {
       baseDir: './public'
     },
-  });
+  },);
 
   watch('./dev/scss/**/*.scss', () => gulp.start('styles'));
   watch('./dev/js/**/*.js', () => gulp.start('scripts',server.reload) );
   watch('./dev/pug/**/*.pug', () => gulp.start('pug', server.reload) );
+  watch('./dev/images/**/*.{png,jpg,jpeg,gif}', () => gulp.start('images') );
+  watch('./dev/images/**/*.svg', () => gulp.start('copy') );
 });
